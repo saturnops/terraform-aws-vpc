7869d43d60850350ed4c70532f9ae4a687888681
Amine Ballalou <ballalouahmedamine@gmail.com>
2022-03-09 09:02:30 +0000

    update

--- outputs.tf
+++ outputs.tf
@@ -15,17 +15,17 @@ output "_3_vpc_cidr_block" {
 
 output "_4_public_subnets" {
   description = "List of IDs of public subnets"
-  value = length(var.public_subnets) > 0 ? module.vpc.public_subnets : null
+  value = length(module.vpc.public_subnets) > 0 ? module.vpc.public_subnets : null
 }
 
 output "_4_private_subnets" {
   description = "List of IDs of private subnets"
-  value = length(var.private_subnets) > 0 ? module.vpc.private_subnets : null
+  value = length(module.vpc.private_subnets) > 0 ? module.vpc.private_subnets : null
 }
 
 output "_6_database_subnets" {
   description = "List of IDs of database subnets"
-  value = length(var.database_subnets) > 0 ? module.vpc.database_subnets : null
+  value = length(module.vpc.database_subnets) > 0 ? module.vpc.database_subnets : null
 }
 
 output "_7_vpn-host-public-ip" {
@@ -50,6 +50,6 @@ output "vpn_server_pem_file" {
 
 output "pritunl_info" {
   description = "Pritunl Info"
-  value       = var.vpn_server_enabled ? "Please check the Pritunl keys and login credentials in 'pritunl-info.txt' file in 'pritunl' folder" : "vpn host is not enabled therefore pritunl not installed" 
+  value       = var.vpn_server_enabled ? "Please check the Pritunl keys and login credentials in 'pritunl-info.txt' file in 'pritunl' folder" : null
 }
 
