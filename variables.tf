2f9b7e7349b475be0bb3aa1115affe7e7cefa20c
Amine Ballalou <ballalouahmedamine@gmail.com>
2022-08-16 09:38:32 +0000

    Skaf 250(Upgrade VPN)

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "vpn_server_enabled" {
 }
 
 variable "vpn_server_instance_type" {
-  description = "EC2 instance Type for VPN Server"
+  description = "EC2 instance Type for VPN Server, Only amd64 based instance type are supported eg. t2.medium, t3.micro, c5a.large etc. "
   default = "t3a.small"
   type    = string
 }
