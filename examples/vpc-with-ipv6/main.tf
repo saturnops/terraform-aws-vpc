2647ea5c3acb77748187976575cf15818b9b61f1
Amine Ballalou <ballalouahmedamine@gmail.com>
2023-06-30 17:50:51 +0530

    added IPv6 support

--- examples/vpc-with-ipv6/main.tf
+++ examples/vpc-with-ipv6/main.tf
@@ -1,6 +1,6 @@
 locals {
   name        = "skaf"
-  region      = "ap-south-1"
+  region      = "us-east-1"
   environment = "stage"
   additional_aws_tags = {
     Owner      = "SaturnOps"
@@ -11,7 +11,7 @@ locals {
 }
 
 module "vpc" {
-  source                 = "../.."
+  source                 = "saturnops/vpc/aws"
   name                   = local.name
   vpc_cidr               = local.vpc_cidr
   environment            = local.environment
